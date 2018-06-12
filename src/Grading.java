3861347bc7d24f98b95d0b9fcc42c593f9b779f4
Unknown <econcelman@gmail.com>
2018-06-11 22:07:13 -0400

    The first draft a fully functioning Program

--- src/Grading.java
+++ src/Grading.java
@@ -3,46 +3,132 @@ import java.util.Scanner;
 
 public class Grading {
 
+	static double A, B, C, D;
+	
 	public static void main(String[] args) {
 		
 		int studentNumber = 0;
 		int answer;
 		ArrayList<Student> studentRoster = new ArrayList<Student>();
 		Scanner scan = new Scanner (System.in);
-
-		System.out.println("How many students do you have in your class?");
-		studentNumber = scan.nextInt();
-		System.out.println();
+		double classTotal = 0;
+		double classMean = 0;
+		double sumSquaredDif = 0;
+		double standardDeviation;
 		
+			
 		System.out.println("Enter the first student's information:");
 		System.out.println();
 		
 		
-		
-		//this should create student objects with names and then collect their exam/assignment
-		//scores and store them in the student object as array lists
+		//this creates student objects with names and then collect their exam/assignment
+		//scores and store them in the student object as array lists studendRoster
 		do {
 			
 				String name;
 				System.out.println("Please enter the student's name:");
-				name = scan.nextLine();
+				name = scan.next();
 				Student student = new Student(name);
-				
+
+				student.getScores(student);
 				studentRoster.add(student);
-				Student.setScores(student);
 				
 				
 				System.out.println("would you like to add another student?");
 				System.out.println("Enter \"1\" to add another student or enter \"2\" to finish");
 				answer = scan.nextInt();
+			
 				
+		//Prints out the current list of student objects with their name, exam scores, 
+		//assignment scores, and their total points
 		}
 		while (answer ==1);
 		
-		//this should print out all the student objects with their names and scores of exams/assignments
-		Student.displayScores(studentRoster);
+		//this prints out all the student objects with their names and exams/assignment scores
+		for(Student student : studentRoster)
+		{
+			System.out.println("Student's Name: " + student.getName());
+			for( int e : student.getExamScores())
+			{
+				System.out.print("Exam Scores:");
+				System.out.println(e);
+				System.out.println();
+			}
+			for(int a : student.getAssignmentScores()) {
+				System.out.print("Assignment Scores:");
+				System.out.println(a);
+				System.out.println();
+			}
+			System.out.print("Total Points: ");
+			System.out.println(student.getTotalPoints());
+			classTotal += student.getTotalPoints();
+			System.out.println();
+		}
 		
-
+		
+		classMean = getClassMean(classTotal, studentRoster.size());
+		
+		//this takes each students total points and subtracts the class average, 
+		//then squares the result.  It then adds up all the results to get the sum
+		//of the squared means
+		for(Student student : studentRoster) {
+			double squareddif = Math.pow(student.getTotalPoints()-classMean, 2);
+			sumSquaredDif += squareddif;
+		}
+		
+		
+		standardDeviation = Math.sqrt(sumSquaredDif/studentRoster.size());
+		
+		A = classMean + (standardDeviation * 2);
+		B = classMean + standardDeviation;
+		C = classMean;
+		D = classMean - standardDeviation;
+		
+		System.out.println("FinalScore >= " + Math.floor(A) + " : A");
+		System.out.println("FinalScore >= " + Math.floor(B) + " : B");
+		System.out.println("FinalScore >= " + Math.floor(C) + " : C");
+		System.out.println("FinalScore >= " + Math.floor(D) + " : D");
+		System.out.println("FinalScore < " + Math.floor(D) + " : F");
+		System.out.println();
+		
+		
+				
+		for(Student student : studentRoster)
+		{
+			System.out.print(student.getName() + ": ");
+			finalClassGrade(student.getTotalPoints());
+			
+		}
+		
+		
+	
 	}
 
+	private static double getClassMean(double classTotal, int classSize) {
+	
+		return classTotal / classSize;	
+	}
+	
+	
+	private static void finalClassGrade(double totalPoints) {
+		if(totalPoints >= A)
+			System.out.println("Final Grade = A");
+		else if (totalPoints >= B)
+			System.out.println("Final Grade = B");
+		else if (totalPoints >= C)
+			System.out.println("Final Grade = C");
+		else if (totalPoints >= D)
+			System.out.println("Final Grade = D");
+		else 
+			System.out.println("Final Grade = F");
+	}
+	
+	
 }
+
+
+
+
+
+
+
--- src/Student.java
+++ src/Student.java
@@ -3,29 +3,39 @@ import java.util.Scanner;
 
 public class Student {
 	
-		static ArrayList<Integer> examScores = new ArrayList<Integer>();
-		static ArrayList<Integer> assignmentScores = new ArrayList<Integer>();
+		ArrayList<Integer> examScores = new ArrayList<Integer>();
+		ArrayList<Integer> assignmentScores = new ArrayList<Integer>();
 		String name;
+		double totalPoints;
+		
+
 
 		public Student(String name) {
-		this.name = name;
+			this.name = name;
+			this.totalPoints = 0;
+			
+		
 		}
 		
-		 public static void setScores(Student student) {
+		//this should set the scores for the array lists in the student object
+		 public void getScores(Student student) {
 			
 			Scanner scan = new Scanner (System.in);
 			String name = "not set";
-			int totalPoints, score;
+			int score;
 			int answer = 1;
+			
+			
 				
 				for (int num = 1; num <10; num++) {
 					if(num<=4) {
 					System.out.println("Enter Exam score # " + (num) + " (between 0 and 50)");
 					score = scan.nextInt();
 						while(score > 50) {
-						System.out.println("Invalid score entry.  Please enter score between 0 - 50");
-						score = scan.nextInt();
+							System.out.println("Invalid score entry.  Please enter score between 0 - 50");
+							score = scan.nextInt();
 						}
+					totalPoints += (double) score;
 					examScores.add(score);
 					}
 					
@@ -36,17 +46,27 @@ public class Student {
  							System.out.println("Invalid score entry.  Please enter score between 0 - 75");
  							score = scan.nextInt();
  							}
-					assignmentScores.add(score);	
+						totalPoints += (double) score;
+						assignmentScores.add(score);	
 					}
 				
 				}
 		 }
 		
-			
-		public static void displayScores(ArrayList<Student> studentRoster) {
-			for (int i = 0; i<= studentRoster.size(); i++) {
-				System.out.println(studentRoster.get(i));
-				}
+		public double getTotalPoints() {
+			return totalPoints;
+		}
+
+		public ArrayList<Integer> getExamScores() {
+			return examScores;
+		}
+
+		public ArrayList<Integer> getAssignmentScores() {
+			return assignmentScores;
+		}
+
+		public String getName() {
+			return name;
 		}
 }
 		
